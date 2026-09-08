a78e73ec2015b5fa1ce2a628dcd4b7c1674c6edc
René F. Navarro <rene.navarro@gmail.com>
2026-09-08 15:48:30 -0700

    Código base para la práctica

---
+++ src/main/java/mx/unison/ProcesadorCalificaciones.java
@@ -0,0 +1,22 @@
+package mx.unison;
+
+import java.util.logging.Logger;
+
+public class ProcesadorCalificaciones {
+
+    private static final Logger LOG =
+            Logger.getLogger(
+                    ProcesadorCalificaciones.class.getName()
+            );
+
+    public static void main(String[] args) {
+
+        LOG.info(
+                "Iniciando ProcesadorCalificaciones"
+        );
+
+        System.out.println(
+                "Procesando archivo..."
+        );
+    }
+}
